280e58abf59e758177f6b3657cf2958920600b0d
DepressedCookie <vipc.1224@live.ca>
2020-12-17 05:04:40 -0500

    Added Audio Manager script

---
+++ Assets/Weather_Package/Scripts/Script_AudioManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_AudioManager : MonoBehaviour
+{
+    [Header("Audio Sources")]
+    [SerializeField] AudioSource AmbientAudio;
+    [SerializeField] AudioSource RainAudio;
+    [SerializeField] AudioSource ThunderAudio;
+    [SerializeField] AudioSource OtherAudio;
+
+
+    [Header("Audio Clips")]
+    [SerializeField] AudioClip clip_birds;
+    [SerializeField] AudioClip clip_wind;
+    [SerializeField] AudioClip clip_lightRian;
+    [SerializeField] AudioClip clip_HeavyRian;
+    [SerializeField] AudioClip clip_CloseTunder;
+    [SerializeField] AudioClip clip_FarTunder;
+
+    Script_AudioManager instance;
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+        if (instance == null) { instance = this; }
+        else { Destroy(this); }
+    }
+
+        // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
